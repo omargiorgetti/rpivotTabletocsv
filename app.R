e746baf59dc7030f2da2d2b0220bb5d1007bd767
Omar Giorgetti <omar.giorgetti@regione.toscana.it>
2019-05-14 14:03:45 +0200

    dinamic create pivot selecting row and column by drag and drop

--- app.R
+++ app.R
@@ -1,25 +1,81 @@
 library(shiny)
+library(dragulaR)
 
+dTitanic<-as.data.frame(Titanic)
+
+#style = "border-width:1px;border-style:solid;"
+makeElement <- function(data, name)
+{
+  if (class(data[[name]])=='numeric'){
+    div(
+      drag = name,
+      div(type="button",class="btn btn-success btn-sm btn-block", name)
+      #div(class = "active content", p(sprintf("Class: %s", class(data[[name]]))))
+    )
+  }else{
+    div(
+      drag = name,
+      div(type="button",class="btn btn-info btn-sm btn-block", name)
+      #div(class = "active content", p(sprintf("Class: %s", class(data[[name]]))))
+      )
+  }
+}
 source("function.R")
 #ui
 ui = fluidPage(
-  downloadButton("downloadData", "Salva csv"),
-  fluidRow( rpivotTable::rpivotTableOutput("pivot")))
+  fluidRow(
+    column(12,
+      fluidRow(
+        column(4,
+          h3("Dimensioni,misure:"),
+          div(class="jumbotron",
+            div(id = "variabili", class="container",
+              lapply(colnames(dTitanic), makeElement, data = dTitanic)
+            )
+          )
+        ),  
+        column(8,
+          fluidRow(     
+            column(4,
+              h3("Righe"),
+              div(id = "righe",class="jumbotron")     
+            ),
+            column(4,
+              h3("Colonne"),
+              div(id = "colonne",class="jumbotron")  
+            )
+          ),
+          fluidRow(
+            downloadButton("downloadData", "Salva csv"),
+            rpivotTable::rpivotTableOutput("pivot"),
+            verbatimTextOutput("print")
+          )
+        )
+      )
+    ) 
+  ),
+  dragulaOutput("sel")
+  
+)
 
 
 #server
 server = function(input, output) { 
+  state<-reactive({dragulaValue(input$sel)})
+  output$sel <- renderDragula({
+    dragula(c("variabili", "colonne","righe"))})
+  
   output$pivot <- rpivotTable::renderRpivotTable(
-    rpivotTable::rpivotTable(mtcars
-      , aggregatorName = "Sum"
-      , rendererName =  "Table"
-      #,rows=c("vs","cyl"),cols=c("am","gear"),vals="mpg"
-      , width="50%"
-      , height="550px"
-      ,onRefresh=htmlwidgets::JS("
+    rpivotTable::rpivotTable(dTitanic
+     , aggregatorName = "Sum"
+     , rendererName =  "Table"
+     ,rows=state()$righe
+     ,cols=state()$colonne
+     , width="50%"
+     , height="550px"
+     ,onRefresh=htmlwidgets::JS("
         function() { 
-          
-          var rend = document.getElementsByClassName('pvtRenderer')['0'].value;
+                    var rend = document.getElementsByClassName('pvtRenderer')['0'].value;
           var misura = document.getElementsByClassName('pvtAttrDropdown')['0'].value;
           var aggr = document.getElementsByClassName('pvtAggregator')['0'].value;
           var source = document.getElementsByClassName('pvtTable')['0'];
@@ -60,7 +116,14 @@ server = function(input, output) {
       )
     )
   )
-  
+  output$print <- renderText({
+    state <- dragulaValue(input$sel)
+        sprintf("Variabili:\n  %s\n\nColonne:\n  %s\n\nRighe:\n  %s",
+            paste(state$variabili, collapse = ", "),
+            paste(state$colonne, collapse = ", "),
+            paste(state$righe, collapse = ", "))
+    
+  })
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("pivot.csv", sep = "")
--- cube.R
+++ cube.R
@@ -2,10 +2,25 @@ library(XML)
 library(Hmisc)
 library(rpivotTable)
 data<-xmlParse(file="Turismo_definitivoultimo.xml")
-print(data)
-ldata<-xmlToList(data)
-names(ldata)
+cubes<-xmlToList(data)
+
+for(cube in cubes){
+  
+  print("------")
+  print(cube)
+  
+}
+  
 a1<-ldata[1]
+a1_1<-a1[[1]][".attrs"][[1]]["name"]
+
+
+a<-list(a=c(1,2,3),b=c(4,5,6))
+a[[1]]
+names(a1)
+a1_1<-a1[[1]]
+c1<-ldata["Cube"]
+c1<-ldata$Cube
 a2<-ldata[2]
 a3<-ldata[3]
 a4<-ldata[4]
@@ -19,8 +34,10 @@ length(a3[[1]])
 length(a4[[1]])
 a4
 print(list.tree(a1))
+datat<-list.tree(a1)
 str(a1)
 library(rpivotTable)
 
 data(HairEyeColor)
 rpivotTable(data = HairEyeColor, rows = "",cols="", vals = "Freq", aggregatorName = "Sum", rendererName = "Table", width="100%", height="400px")
+
